e59be2a7932d1ddf42c747bc3b6afe1392c3a9d0
Ekiserrepé <ekiserrepe@gmail.com>
2024-05-05 14:15:08 +0200

    Update color badge

--- background.js
+++ background.js
@@ -1,6 +1,22 @@
 chrome.runtime.onInstalled.addListener(() => {
-  function updateBadge(price) {
-    chrome.action.setBadgeText({ text: xrpPrice.toString() });
+  let originalBadgeColor = '';
+   function updateBadge(price) {
+    chrome.action.setBadgeText({ text: price.toString() });
+
+
+     if (originalBadgeColor === '') {
+      chrome.action.getBadgeBackgroundColor({}, result => {
+        originalBadgeColor = result;
+      });
+    }
+
+
+    chrome.action.setBadgeBackgroundColor({ color: [255, 215, 0, 255] }); 
+
+    // Después de un breve período de tiempo, restaura el color original de la badge
+    setTimeout(() => {
+      chrome.action.setBadgeBackgroundColor({ color: originalBadgeColor });
+    }, 1000); // 1000 milisegundos (1 segundo)
   }
 
   function fetchXrpPrice() {
@@ -9,7 +25,9 @@ chrome.runtime.onInstalled.addListener(() => {
       .then(data => {
          const xrpPrice = parseFloat(data.result.XXRPZUSD.c[0]);
          const formattedPrice = `${xrpPrice.toFixed(3)}`;
-        chrome.action.setBadgeText({ text: `${formattedPrice}` });
+        //chrome.action.setBadgeText({ text: `${formattedPrice}` });
+        console.log(`Llamo a la función con precio ${formattedPrice}`);
+        updateBadge(formattedPrice);
       })
       .catch(error => {
         console.error('Error fetching XRP price:', error);
@@ -19,6 +37,6 @@ chrome.runtime.onInstalled.addListener(() => {
 
   fetchXrpPrice(); 
 
-  
+
   setInterval(fetchXrpPrice, 60000); 
 });
